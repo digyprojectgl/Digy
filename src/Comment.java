d4492b5311897bf1dff637d71711e882a8708824
ArthurJamelot <arthur.jamelot@gmail.com>
2016-02-27 11:26:42 +0100

    En cours de fabrication du diagramme, avec les fichiers cette fois ci

---
+++ src/Comment.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Arthur on 26/02/2016.
+ */
+public class Comment {
+    private java.util.Date dateComment;
+    public String message;
+    private Entry targetEntry;
+
+    public Comment method(String content) {
+        return null;
+    }
+}
